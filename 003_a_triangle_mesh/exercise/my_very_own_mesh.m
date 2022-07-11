2f301fdd1333e735cd25035dccb173c0b26a0559
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-07-11 17:41:48 -0300

    003 exercises

--- 003_a_triangle_mesh/exercise/my_very_own_mesh.m
+++ 003_a_triangle_mesh/exercise/my_very_own_mesh.m
@@ -1,14 +1,51 @@
 function [V,F] = my_very_own_mesh()
-%MY_VERY_OWN_MESH  construct my very own triangle mesh
+% MY_VERY_OWN_MESH  construct my very own triangle mesh
 %
-% [V,F] = my_very_own_mesh();
+%   [V, F] = my_very_own_mesh();
 %
-% Outputs:
-%  V,F  a triangle mesh for which creativity knows no bounds
+%   Output:
+%       V, F : a triangle mesh for which creativity knows no bounds
 
-V = [  ];
+V = [
+    0, 0, 0;
+    4, 0, 0;
+    4, 4, 0;
+    0, 4, 0;
+    0, 0, 1;
+    4, 0, 1;
+    4, 4, 1;
+    0, 4, 1;
+    1, 1, 2;
+    3, 1, 2;
+    3, 3, 2;
+    1, 3, 2;
+];
 
-F = [  ];
+F = [
+    2, 4, 1;
+    2, 3, 4;
+
+    1, 2, 6;
+    1, 6, 5;
+    2, 3, 7;
+    2, 7, 6;
+    3, 4, 8;
+    3, 8, 7;
+    4, 1, 5;
+    4, 5, 8;
+
+    5, 10, 9;
+    5, 6, 10;
+    6, 11, 10;
+    6, 7, 11;
+    7, 12, 11;
+    7, 8, 12;
+    8, 9, 12;
+    8, 5, 9;
+
+    10, 12, 9;
+    10, 11, 12;
+];
 
 end
 
