7b950fa329379c28cd0880590a7be18ea873031a
Tiago de Souza Fernandes <tiagotsf2000@gmail.com>
2022-07-14 14:03:46 -0300

    008 normal

--- 008_normals/exercise/my_normals.m
+++ 008_normals/exercise/my_normals.m
@@ -1,15 +1,17 @@
 function N = my_normals(V,F)
-  % MY_NORMALS Compute *unnormalized* normals per face
-  %
-  % N = normals(V,F)
-  % N = normals(V,F,'ParameterName',ParameterValue, ...)
-  %
-  % Inputs:
-  %  V  #V x 3 matrix of vertex coordinates
-  %  F  #F x 3  matrix of indices of triangle corners
-  % Output:
-  %  N  #F x 3 list of face normals
+% MY_NORMALS Compute *unnormalized* normals per face
+%
+% N = normals(V,F)
+% N = normals(V,F,'ParameterName',ParameterValue, ...)
+%
+% Inputs:
+%  V  #V x 3 matrix of vertex coordinates
+%  F  #F x 3  matrix of indices of triangle corners
+% Output:
+%  N  #F x 3 list of face normals
   
-  N = ...
+    N = zeros(length(F), 3);
+    for i = 1:length(F)
+        N(i, :) = cross(V(F(i, 2), :) - V(F(i, 1), :), V(F(i, 3), :) - V(F(i, 1), :));
   
 end
